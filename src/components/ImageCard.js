c5da093d732950a2db15e3d01050ef0fc1b53c64
Wonmi <wonmadesign@gmail.com>
2020-04-12 03:34:19 -0500

    Set up React Ref to ImageCard Component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         const { urls, description } = this.props.image;
 
         return (
-            <img    alt={description} src={urls.regular} />
+            <img ref={this.imageRef} alt={description} src={urls.regular} />
         );
     }
 }
