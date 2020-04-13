4fa524e921ae4d5f9cbd8dd19f05ba4f8a47c830
Wonmi <wonmadesign@gmail.com>
2020-04-12 21:36:33 -0500

    Finished ImageSearch App

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,26 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
         this.imageRef = React.createRef();
+        this.state = { span: null };
     }
 
     componentDidMount() {
-        console.log(this.imageRef.current.clientHeight);
+        // console.log(this.imageRef.current.clientHeight);
+        this.imageRef.current.addEventListener('load', this.setSpan);
+    }
+
+    setSpan = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 2) + 10;
+
+        this.setState({ span: spans });
     }
 
     render() {
         const { urls, description } = this.props.image;
 
         return (
-            <img ref={this.imageRef} alt={description} src={urls.regular} />
+            <img style={{ gridRowEnd: `span ${this.state.span}`}} ref={this.imageRef} alt={description} src={urls.regular} />
         );
     }
 }
